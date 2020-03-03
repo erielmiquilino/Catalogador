469240e04a15f85e40df9ad855cc45ffe549b6e4
Eriel Miquilino <erielmiquilino@hotmail.com>
2020-03-03 01:33:28 -0300

    Removido mapstruct e adicionado modelmapper para mapear executar o mapeamento entre objeto

--- src/main/java/org/catalogador/services/MetadataServiceImpl.java
+++ src/main/java/org/catalogador/services/MetadataServiceImpl.java
@@ -1,16 +1,17 @@
 package org.catalogador.services;
 
-import org.catalogador.domains.models.Result;
+import org.catalogador.domains.models.omdb.Search;
 import org.catalogador.domains.models.Title;
-import org.catalogador.mappers.TitleMapper;
 import org.catalogador.services.interfaces.MetadataService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -22,14 +23,9 @@ public class MetadataServiceImpl implements MetadataService {
     @Value("${omdbapi.apikey}")
     private String apiKay;
 
-    private final TitleMapper titleMapper;
-
-    @Autowired
-    public MetadataServiceImpl(TitleMapper titleMapper) {
-        this.titleMapper = titleMapper;
-    }
-
     public Flux<Title> searchMetadata(String title) {
+        ModelMapper modelMapper = new ModelMapper();
+
         RestTemplate restTemplate = new RestTemplate();
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
@@ -38,9 +34,12 @@ public class MetadataServiceImpl implements MetadataService {
                 .queryParam("type", "movie")
                 .queryParam("page", "1");
 
-        var results = restTemplate.getForObject(builder.toUriString(), Result.class);
+        var searchList = restTemplate.getForObject(builder.toUriString(), Search.class);
 
-        var titles = titleMapper.map.searchesToTitles(Objects.requireNonNull(results).getSearch());
+        if (searchList == null || searchList.getSearchItems() == null)
+            return Flux.empty();
+
+        List<Title> titles = modelMapper.map(searchList.getSearchItems(), new TypeToken<List<Title>>(){}.getType());
 
         return Flux.fromIterable(titles);
     }
